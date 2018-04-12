54ab2ef3b63063e1a47a74a1c39e90d8d4dc89ed
myeonghyeon <jeongmyeonghyeon@gmail.com>
2018-04-12 17:33:03 +0900

    Add index.html

---
+++ todoApp.js
@@ -0,0 +1,46 @@
+let todoList = $("#todoList");
+let itemId = 0;
+$("#inputItemText").focus();
+
+$("#addItemBtn").click(function() {
+  itemId += 1;
+
+  let inputItemText = $("#inputItemText").val();
+
+  // create todoItem
+  let editItemBtn = '<button class="editItemBtn" onclick=editItem(' + itemId + ') data-toggle="modal" data-target="#editModal">Edit</button>';
+  let deleteItemBtn = '<button class="deleteItemBtn" onclick=deleteItem(this)>Delete</button>';
+  let todoItem = '<li><span id=todoText' + itemId + '>' + inputItemText + '</span><div style="float:right;">' + editItemBtn + deleteItemBtn + '</div></li>';
+
+  if (inputItemText === "") {
+    alert("텍스트를 입력해주세요!");
+  } else {
+    todoList.append(todoItem);
+  }
+
+  $("#inputItemText").focus();
+  $("#inputItemText").val("");
+});
+
+function focus() {
+$("#editText").focus();
+}
+
+function editItem(itemId) {
+  $("#saveChangesBtn").val(itemId);
+  setTimeout(focus, 400);
+}
+
+function saveChanges(element) {
+  let itemId = element.value;
+  let editItem = $("#todoText"+itemId)[0];
+
+  editItem.textContent = $("#editText").val();
+
+  $("#editText").val("");
+  $("#editModal").modal('hide');
+}
+
+function deleteItem(element) {
+  element.parentElement.parentElement.remove();
+}
